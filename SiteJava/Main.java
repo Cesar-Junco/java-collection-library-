bfbf5d187a58a86152f5e647d6226d8bf430a11d
Cesar-Junco <cesarjuncosanchez@gmail.com>
2025-08-27 09:11:18 -0300

    Adicionando projeto inicial de Collections (List, Set, Map)

---
+++ SiteJava/Main.java
@@ -0,0 +1,22 @@
+class Main{
+    public static void main(String[] args) {
+       int diasSemana= 1;
+       switch (diasSemana){
+        
+            case 2 :
+            case 3 :
+            case 4 :
+            case 5 :
+            case 6 :
+            System.out.println("dia de semana !");
+            break;
+            case 1 :
+            case 7 :
+            System.out.println("fds!");
+            break;
+            default:
+            System.out.println("dia desconhecido desconhecido!");
+       }
+        
+    }
+}
